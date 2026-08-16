2c643f08b95782e436ea0431f8947c69e32c0777
DataDrivenMed <ramparagi@gmail.com>
2026-08-16 11:31:19 -0500

    Route product entry to executive clinical workspace

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 import streamlit as st
 
 st.set_page_config(page_title="Tumor Board Intelligence", page_icon="🧬", layout="wide")
-st.switch_page("pages/00_Mission_Control.py")
+st.switch_page("pages/00_Clinical_Workspace.py")
